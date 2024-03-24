00ca2b3ecf664f39cb50523e536ec4bbc524370d
Krys <krysbaum@gmail.com>
2024-03-24 13:26:06 -0700

    Add create action

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,4 +3,16 @@ class AdventuresController < ApplicationController
     @adventures = Adventure.all
     render :index
   end
+
+  def create
+    @adventure = Adventure.create(
+      name: params[:name],
+      description: params[:description],
+      rating: params[:rating],
+      location: params[:location],
+      category: params[:category],
+      date: params[:date],
+    )
+    render :show
+  end
 end
--- test/controllers/adventures_controller_test.rb
+++ test/controllers/adventures_controller_test.rb
@@ -8,4 +8,10 @@ class AdventuresControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Adventure.count, data.length
   end
+  test "create" do
+    assert_difference "Adventure.count", 1 do
+      post "/adventures.json", params: { name: "hello world", description: "hi hi hi", rating: 5 }
+      assert_response 200
+    end
+  end
 end
